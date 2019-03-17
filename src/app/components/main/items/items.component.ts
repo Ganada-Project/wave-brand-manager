aa87ac34f547865f6b8ed7186aa72b89156f2694
ShinHyunJong <pleiades@algorithmlabs.co.kr>
2019-03-17 16:46:23 +0900

    Item list page 작업중

--- src/app/components/main/items/items.component.ts
+++ src/app/components/main/items/items.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-items',
-  templateUrl: './items.component.html',
-  styleUrls: ['./items.component.scss']
+  selector: "app-items",
+  templateUrl: "./items.component.html",
+  styleUrls: ["./items.component.scss"]
 })
 export class ItemsComponent implements OnInit {
   public balance: number = 365065455;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    console.log("item-list");
   }
-
 }
