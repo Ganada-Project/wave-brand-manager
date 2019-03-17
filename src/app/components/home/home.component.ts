a1c0e8591e1b82b23e4da7847b35325c8cc45785
ShinHyunJong <pleiades@algorithmlabs.co.kr>
2019-03-17 16:44:14 +0900

    create develop branch

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Component, OnInit } from "@angular/core";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private title: Title) { }
+  constructor(private title: Title) {}
 
   ngOnInit() {
-    this.title.setTitle('home');
+    this.title.setTitle("home");
   }
-
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,25 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
-import { Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"]
 })
 export class MainComponent implements OnInit {
-
   public level: number = 10;
 
-  constructor(private auth: AuthService, private router: Router) {
-  }
+  constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit() {
+    console.log("develop branch");
   }
-
 
   logout() {
     this.auth.logout();
-    this.router.navigate(['/']);
+    this.router.navigate(["/"]);
   }
 }
