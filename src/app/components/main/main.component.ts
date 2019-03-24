8769bc26f6799517654d347ca5adfb0c6a683f4d
ShinHyunJong <pleiades@algorithmlabs.co.kr>
2019-03-24 17:11:44 +0900

    selected url

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
-import { navConfigs } from './navConfig';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+import { navConfigs } from "./navConfig";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.scss"]
 })
 export class MainComponent implements OnInit {
   public selectedType: string;
   public navConfigs: any = navConfigs;
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+    this.checkIsSelectedByUrl();
   }
-
 
   onClickNavItem({ type }) {
     this.selectedType = type;
@@ -28,7 +28,13 @@ export class MainComponent implements OnInit {
     console.log("onChange");
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
-    
+  }
+
+  checkIsSelectedByUrl() {
+    const url = location.pathname;
+    const mainTypeArr = url.split("/");
+    const targetType = mainTypeArr[1];
+    this.selectedType = targetType;
   }
 
   checkLoggedIn() {
